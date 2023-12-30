cdb47a72c69a2525aa1c2e5f5d740df4303a46ee
crossroads30 <crossroads30@mail.ru>
2023-12-30 22:05:36 +0300

    feat; add component elements

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -2,18 +2,25 @@ import React from 'react'
 
 const Menu = ({ menuItems }) => {
 	return (
-		<article className='menu'>
+		<section className='section-center'>
 			{menuItems.map(item => {
 				const { id, title, category, price, img, desc } = item
 				return (
-					<div key={id} className='menu-item'>
-						<div className='photo'>
-							<img src={img} alt='' />
+					<article key={id} className='menu-item'>
+						<div>
+							<img className='photo' src={img} alt={title} />
 						</div>
-					</div>
+						<div className='item-info'>
+							<header>
+								<h4>{title}</h4>
+								<span className='price'>{price}</span>
+							</header>
+							<p className='item-text'>{desc}</p>
+						</div>
+					</article>
 				)
 			})}
-		</article>
+		</section>
 	)
 }
 
